c35ed7ce58a5b190a15b9a11a9a2b156710eacca
Trung Nguyen <trung.nguyen@codelink.io>
2023-07-28 00:35:24 +0700

    Initial Commit

---
+++ RMIT-Contact/RMIT_ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_ContactApp.swift
+//  RMIT-Contact
+//
+//  Created by Trung Nguyen on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
